66aa04c2b662fcf4db1c39c7ea48bab40cbb7c32
cngvng <cngvng123@gmail.com>
2024-03-11 13:09:32 +0900

    update extractive summarization

--- model/sbert.py
+++ model/sbert.py
@@ -15,5 +15,5 @@ df_dataset = pd.read_csv("dataset/process/mews-mds.csv")
 print("====== Extractive Summarization ======")
 extractive_model = SBertSummarizer('paraphrase-MiniLM-L6-v2')
 df_test = df_dataset[:100]
-df_test["extractive_summary"] = df_test["document"].apply(lambda x: extractive_summary(x, model=extractive_model, ratio=0.2))
-df_test.to_csv("dataset/final/news-mds-extractive.csv", index=False)
+df_test["extractive_summary"] = df_test["document"].apply(lambda x: extractive_summary(x, model=extractive_model, ratio=0.008))
+df_test.to_csv("dataset/final/news-mds-extractive_008.csv", index=False)
